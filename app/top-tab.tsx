22e87c69c96a8d3acae86db211e0ad792efdcb66
Chandrajeet Maurya <cjmling@gmail.com>
2025-06-12 16:18:11 +0700

    Update TopTab component styles for improved visual consistency and user experience, including background color changes and enhanced text styling for tab indicators.

--- app/top-tab.tsx
+++ app/top-tab.tsx
@@ -6,14 +6,44 @@ export default function TopTabScreen() {
   return (
     <TopTab
       screens={[
-        <View key="1" style={{ flex: 1, backgroundColor: "red" }}>
-          <Text>1</Text>
+        <View
+          key="1"
+          style={{
+            flex: 1,
+            backgroundColor: "#333",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 96, fontWeight: "bold" }}>
+            1
+          </Text>
         </View>,
-        <View key="2" style={{ flex: 1, backgroundColor: "blue" }}>
-          <Text>2</Text>
+        <View
+          key="2"
+          style={{
+            flex: 1,
+            backgroundColor: "#444",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 96, fontWeight: "bold" }}>
+            2
+          </Text>
         </View>,
-        <View key="3" style={{ flex: 1, backgroundColor: "green" }}>
-          <Text>3</Text>
+        <View
+          key="3"
+          style={{
+            flex: 1,
+            backgroundColor: "#555",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text style={{ color: "#fff", fontSize: 96, fontWeight: "bold" }}>
+            3
+          </Text>
         </View>,
       ]}
     />
