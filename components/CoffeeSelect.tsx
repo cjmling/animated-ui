adda9fc32166948400c96ae15f374164f6dbdd43
Chandrajeet Maurya <cjmling@gmail.com>
2025-06-19 13:55:08 +0700

    Update CoffeeSelect component: increase carousel item height for improved visual effects and update coffee images for enhanced presentation.

--- components/CoffeeSelect.tsx
+++ components/CoffeeSelect.tsx
@@ -201,7 +201,7 @@ function CarouselItem({
   const imageAnimatedStyle = useAnimatedStyle(() => {
     return {
       width: CAROUSEL_ITEM_SIZE,
-      height: CAROUSEL_ITEM_SIZE * 1.2,
+      height: CAROUSEL_ITEM_SIZE * 1.3,
       blurRadius: interpolate(
         scrollIndex.value,
         [index - 1, index, index + 1],
