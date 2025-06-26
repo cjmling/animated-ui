659d51e6e36ccd27d49be6045523d04670922cbb
Chandrajeet Maurya <cjmling@gmail.com>
2025-06-26 15:38:33 +0700

    Add background color to StackCards component: set backgroundColor to #222 for improved visual contrast and aesthetics.

--- components/StackCards.tsx
+++ components/StackCards.tsx
@@ -122,6 +122,7 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "#222",
   },
   card: {
     position: "absolute",
