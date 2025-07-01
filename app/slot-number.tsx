968175e46d6b9feb4f22d2b0678c3bc3e6ff4ff2
Chandrajeet Maurya <cjmling@gmail.com>
2025-07-01 17:56:39 +0700

    Refactor SlotNumberScreen to remove automatic number generation and add interactive buttons for incrementing and doubling the number; enhance layout with improved styling for better user experience.

--- app/slot-number.tsx
+++ app/slot-number.tsx
@@ -1,19 +1,49 @@
 import { SlotNumber } from "@/components/SlotNumber";
-import { useEffect, useState } from "react";
-import { View } from "react-native";
+import { useState } from "react";
+import { Pressable, Text, View } from "react-native";
 
 export default function SlotNumberScreen() {
   const [number, setNumber] = useState(123);
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setNumber(Math.floor(Math.random() * 1000));
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setNumber(Math.floor(Math.random() * 1000));
+  //   }, 1000);
+  //   return () => clearInterval(interval);
+  // }, []);
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <SlotNumber number={number} />
+      <View style={{ flexDirection: "row", gap: 10, marginTop: 20 }}>
+        <Pressable
+          style={{
+            borderColor: "#CCC",
+            borderWidth: 1,
+            borderRadius: 10,
+            padding: 10,
+            width: 50,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => setNumber(number + 1)}
+        >
+          <Text>+</Text>
+        </Pressable>
+        <Pressable
+          style={{
+            borderColor: "#CCC",
+            borderWidth: 1,
+            borderRadius: 10,
+            padding: 10,
+            width: 50,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+          onPress={() => setNumber(number * 2)}
+        >
+          <Text>x 2</Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
