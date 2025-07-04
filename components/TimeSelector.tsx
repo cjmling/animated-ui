fd461645b047fc21d9e61445a76e3ae5dbab8f02
Chandrajeet Maurya <cjmling@gmail.com>
2025-07-04 15:49:10 +0700

    Update TimeSelector component to manage selected time state and enhance time selection options with structured hour and minute values; modify CircularClock and VerticalScrollSelect to reflect the selected time accurately.

--- components/TimeSelector.tsx
+++ components/TimeSelector.tsx
@@ -1,14 +1,31 @@
+import { useState } from "react";
 import { View } from "react-native";
 import { CircularClock } from "./CircularClock";
 import { VerticalScrollSelect } from "./VerticalScrollSelect";
 
 export const TimeSelector = () => {
+  const [selectedTime, setSelectedTime] = useState<{
+    hour: number;
+    minute: number;
+  }>({ hour: 12, minute: 0 });
   const SELECT_LABELS = [
-    { label: "12:00", value: 0 },
-    { label: "1:00", value: 1 },
-    { label: "2:00", value: 2 },
-    { label: "3:00", value: 3 },
-    { label: "4:00", value: 4 },
+    { label: "12:00", value: { hour: 12, minute: 0 } },
+    { label: "12:30", value: { hour: 12, minute: 30 } },
+    { label: "1:00", value: { hour: 1, minute: 0 } },
+    { label: "1:30", value: { hour: 1, minute: 30 } },
+    { label: "2:00", value: { hour: 2, minute: 0 } },
+    { label: "2:30", value: { hour: 2, minute: 30 } },
+    { label: "3:00", value: { hour: 3, minute: 0 } },
+    { label: "3:30", value: { hour: 3, minute: 30 } },
+    { label: "4:00", value: { hour: 4, minute: 0 } },
+    { label: "4:30", value: { hour: 4, minute: 30 } },
+    { label: "5:00", value: { hour: 5, minute: 0 } },
+    { label: "5:30", value: { hour: 5, minute: 30 } },
+    { label: "6:00", value: { hour: 6, minute: 0 } },
+    { label: "6:30", value: { hour: 6, minute: 30 } },
+    { label: "7:00", value: { hour: 7, minute: 0 } },
+    { label: "7:30", value: { hour: 7, minute: 30 } },
+    { label: "8:00", value: { hour: 8, minute: 0 } },
   ];
   return (
     <View
@@ -23,8 +40,8 @@ export const TimeSelector = () => {
       }}
     >
       <CircularClock
-        hour={2}
-        minute={30}
+        hour={selectedTime.hour}
+        minute={selectedTime.minute}
         backgroundColor="#212121"
         lineColor="#555"
         armColor="#fff"
@@ -35,7 +52,9 @@ export const TimeSelector = () => {
       />
       <VerticalScrollSelect
         selected={0}
-        setSelected={() => {}}
+        setSelected={(index) => {
+          setSelectedTime(SELECT_LABELS[index].value);
+        }}
         labels={SELECT_LABELS}
       />
     </View>
